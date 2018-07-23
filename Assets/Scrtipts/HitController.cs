75274cc65d125719d4751177dbb541718952bdfe
Scaut <muchroom4427@yandex.ru>
2018-07-23 14:51:19 +0300

    Initial commit

---
+++ Assets/Scrtipts/HitController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HitController : MonoBehaviour {
+
+    public Text HpText;
+    public Text WoodText;
+    public int Hp = 100;
+    public int Wood = 0;
+
+   // Random rnd = new Random();
+   // int value = rnd.Next();
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnMouseDown()
+    {
+        Hp -= 10;
+        if(Hp == 0)
+        {
+            Hp = 100;
+            Wood += 10;
+        }
+
+        HpText.text = "HP: " + Hp;
+        WoodText.text = "Wood: " + Wood;
+    }
+}
