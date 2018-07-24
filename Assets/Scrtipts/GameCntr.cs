484897bb08aadfed529b94b1915b8d73bc69efca
PinDanil <drobrorobro@gmail.com>
2018-07-24 20:12:37 +0300

    Add some features
    Заставил все двигаться согласовано
    Добавил несколько видов деревьев с разным хп и количеством дерева при вырубке

---
+++ Assets/Scrtipts/GameCntr.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameCntr : MonoBehaviour {
+
+    public HitController Tree;//связь скриптов
+    public GameObject firstTree;// первое дерево необходимо, чтобы включить коллайдер при открытии сцены
+    public bool isDead = false;// говорим, что все живы здоровы 
+    public int Wood = 0;
+    public int moveSpeed;
+
+    // Use this for initialization
+    void Start () {
+        firstTree.GetComponent<CapsuleCollider2D>().enabled = true;//включаем первому дерево коллайдер 
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+
+	}
+}
--- Assets/Scrtipts/HitController.cs
+++ Assets/Scrtipts/HitController.cs
@@ -3,54 +3,105 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class HitController : MonoBehaviour {    
+public class HitController : MonoBehaviour {
 
+    public GameCntr GameCntr;// связь с скриптом GameCntr
+    public GameObject stump;//  префаб пня. Нужен, чтобы спавнить его при смерти дерева
     public Text HpText;
     public Text WoodText;
-    public int Hp = 100;
-    public int Wood = 0;
+    public int Hp;
+    public string type; // Тип дерева
+ //   private Color32 red = new Color32(1,0,0,1);
+//    private Color32 none = new Color32(1, 1, 1, 1);
 
-    public bool isDead = false;
-
-
-   // Random rnd = new Random();
-   // int value = rnd.Next();
+    public static System.Random rnd = new System.Random();//создаем объект класса рандом из библиотек sistem т.к. у юнити он всосный
+    private int typeNumber;// номет типа дерева 
 
     // Use this for initialization
     void Start () {
-		
-	}
+        
+        GetComponent<CapsuleCollider2D>().enabled = false;// убираем коллайдер при спавне, чтобы нельзя было бить правое дерео
+        typeNumber = rnd.Next(1, 4);// рандомим номер типа дерева
+        switch (typeNumber)// обрабатываем то, что натворил рандом
+        {
+            case 1://Oak
+                type = "Oak";
+                Hp = 110;
+                GetComponent<SpriteRenderer>().sprite = Resources.Load<Sprite>("Sprites/Trees/Oak");//подгружаем спрайт дуба\сакуры\березы
+                break;
+            case 2://Sakura
+                type = "Sakura";
+                Hp = 90;
+                GetComponent<SpriteRenderer>().sprite = Resources.Load<Sprite>("Sprites/Trees/Sakura");
+                break;
+            case 3://Broch
+                type = "Brich";
+                Hp = 70;
+                GetComponent<SpriteRenderer>().sprite = Resources.Load<Sprite>("Sprites/Trees/Brich");
+                break;
+            default:
+                type = "Brich";
+                Hp = 70;
+                GetComponent<SpriteRenderer>().sprite = Resources.Load<Sprite>("Sprites/Trees/Brich");
+                break;
+
+        }
+
+    }
 	
 	// Update is called once per frame
 	void Update () {
 
-       if(isDead)
+       if(GameCntr.isDead)// при смерти среднего/текущего/main дерева
         {
-            transform.Translate(-8 * Time.deltaTime, 0, 0);
-            
+            transform.Translate(GameCntr.moveSpeed * Vector2.left * Time.deltaTime);//говорим дереву(уже правому) двигаться влево со скоростью moveSpeed    
         }
-        else
+
+       if (transform.position.x < 0)// когда правое дерево встает в центр (условно становится среднем/текущем/main )
         {
-            if (transform.position.x > 0) transform.position = new Vector3(0, 0, 0);
+            transform.position = new Vector3(0,0,0);//кидаем его в центр сцены
+            transform.Translate(Vector2.zero);//говорим ему не двигаться (на всякий)
+            GetComponent<CapsuleCollider2D>().enabled = true;// включаем коллайдер, чтобы его можно было рубить 
+            GameCntr.isDead = false;// сообщаем, что main дерево живо
         }
 		
 	}
 
-    void OnMouseDown()
+    void OnMouseDown()// метод обрабатывает клик по коллайдеру дерева
     {
         Hp -= 10;
-        if(Hp == 0)
-        {
-            Hp = 100;
-            Wood += 10;
-            
-            transform.position = new Vector3(12,0,0);
 
-            isDead = true;
+ //       GetComponent<SpriteRenderer>().color = red;
+ //       GetComponent<SpriteRenderer>().color = none;
+
+        if (Hp == 0)// ХЭПЭ равно нулю- фактическая смерть
+        {
+            switch (typeNumber)
+            {
+                case 1:
+                    GameCntr.Wood += 15;// добавляем дерево
+                    break;
+                case 2:
+                    GameCntr.Wood += 10;// добавляем дерево
+                    break;
+                case 3:
+                    GameCntr.Wood += 8;// добавляем дерево
+                    break;
+            }
+
+            GameCntr.Wood += 10;// добавляем дерево
+
+            Instantiate(this.gameObject, new Vector3(12, 0, 0), Quaternion.identity);//спавним новое правое дерево
+
+            Instantiate(stump,new Vector3(0,0,0), Quaternion.identity); //спавним пень в центре сцены
+
+            GameCntr.isDead = true;//говорим, что main дерево мертво
+
+            Destroy(this.gameObject);//уничтожаем себя (main дерево)
         }
 
         HpText.text = "HP: " + Hp;
-        WoodText.text = "Wood: " + Wood;
+        WoodText.text = "Wood: " + GameCntr.Wood;
 
 
     }
